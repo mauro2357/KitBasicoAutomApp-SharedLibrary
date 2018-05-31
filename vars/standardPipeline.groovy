fa043aace46c4e6b1ab60a877bafc8cc7c13655c
Mauricio Giraldo <mauro2357@gmail.com>
2018-05-31 07:42:41 -0500

    no message

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -1,13 +1,19 @@
 def deploy(url) {
      echo "INFO: ${url}"
 	 
+	 //Descarga la configuración de despliegue a producción desde git
 	 checkout([$class: 'GitSCM', 
 						branches: [[name: '*/master']], 
 						doGenerateSubmoduleConfigurations: false, 
 						extensions: [[$class: 'RelativeTargetDirectory', 
 							relativeTargetDir: 'KitBasicoAutomApp-Ops']], 
 						submoduleCfg: [], 
-						userRemoteConfigs: [[url: 'https://github.com/mauro2357/KitBasicoAutomApp-Ops.git']]]) 
+						userRemoteConfigs: [[url: 'https://github.com/mauro2357/KitBasicoAutomApp-Ops.git']]])
+						
+     bat 'mkdir "KitBasicoAutomApp/build/libs/config"'
+					bat 'xcopy "KitBasicoAutomApp-Ops/config" "KitBasicoAutomApp/build/libs/config"'
+					bat "deploy-bd.bat"
+					bat "deploy-app.bat"
 	 
 	 // Clean workspace after doing anything
 	 //deleteDir()
